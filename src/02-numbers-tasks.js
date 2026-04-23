11572226ac9fa2f24ea53bce8cb12ab889bdd78d
Nataliya Krylova <nataliya.krylova2022@gmail.com>
2026-04-23 15:22:07 +0200

    feat: resolve number tasks

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -199,8 +199,12 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n < 2) return false;
+  for (let i = 2; i <= n / 2; i += 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
@@ -218,8 +222,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return Number.isNaN(Number(value)) ? def : Number(value);
 }
 
 module.exports = {
